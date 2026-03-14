a35076e7b24fbd357c4429575cfe5ccef928eae7
WanDly <wzhwangdejxz@outlook.com>
2026-03-14 12:44:11 +0800

    Add custom theme for VitePress documentation

---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,10 @@
+import DefaultTheme from 'vitepress/theme'
+import './style.css'
+
+export default {
+  extends: DefaultTheme,
+  
+  enhanceApp({ app, router, siteData }) {
+    // 可以在这里注册全局组件或添加自定义逻辑
+  }
+    }
